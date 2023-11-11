ec8f642e59c1e8ffb6db3df9a3f2a894fa6716e9
Yunjin <jed.donaire08@gmail.com>
2023-11-12 01:26:02 +0800

    Added and Modified Search Bar with Tags

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,6 +12,11 @@ const Nav = () => {
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
+  const handleLogoClick = () => {
+    // Your additional logic if needed
+    router.reload();
+  };
+
   useEffect(() => {
     (async () => {
       const res = await getProviders();
@@ -21,7 +26,11 @@ const Nav = () => {
 
   return (
     <nav className="flex-between w-full mb-16 pt-3">
-      <Link href="/" className="flex gap-2 flex-center">
+      <Link
+        href="/"
+        className="flex gap-2 flex-center"
+        onClick={handleLogoClick}
+      >
         <Image
           src="/assets/images/logo2.svg"
           alt="logo"
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -12,6 +12,10 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   const pathName = usePathname();
   const router = useRouter();
 
+  const tag_string = post.tag;
+  const tags = tag_string.split(" "); // Assuming tags are separated by spaces
+  console.log(tags);
+
   const [copied, setCopied] = useState("");
 
   const handleCopy = () => {
@@ -57,14 +61,19 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         </div>
       </div>
       <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
-      <p
-        className="font-inter text-sm blue_gradient cursor-pointer"
-        onClick={() => {
-          handleTagClick && handleTagClick(post.tag);
-        }}
-      >
-        {post.tag}
-      </p>
+      <div className="flex space-x-2 font-inter text-sm blue_gradient cursor-pointer">
+        {tags.map((tag) => (
+          <p
+            key={tag}
+            className="font-inter text-sm blue_gradient cursor-pointer"
+            onClick={() => {
+              handleTagClick && handleTagClick(tag);
+            }}
+          >
+            {tag}
+          </p>
+        ))}
+      </div>
 
       {session?.user.id === post.creator._id && pathName === "/profile" && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
