8d16eb73824f431bc53e29a1175ec5a02a78ece4
Yunjin <jed.donaire08@gmail.com>
2023-11-15 12:11:10 +0800

    Adjusted the Page Adjustments

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -14,7 +14,6 @@ const Nav = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const handleLogoClick = () => {
-    // Your additional logic if needed
     router.reload();
   };
 
@@ -29,7 +28,7 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link
         href="/"
-        className="flex gap-2 flex-center"
+        className="flex gap-2 flex-center py-1"
         onClick={handleLogoClick}
       >
         <Image
